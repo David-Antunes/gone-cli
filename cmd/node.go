a988a6ccfc9616add86b72b4fe0393d1d749fd12
David Antunes <dmm.antunes@campus.fct.unl.pt>
2024-10-21 14:51:31 +0100

    Fix docker string

--- cmd/node.go
+++ cmd/node.go
@@ -4,10 +4,10 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"net/http"
+
 	api "github.com/David-Antunes/gone/api/Add"
 	"github.com/spf13/cobra"
-	"net/http"
-	"strings"
 )
 
 // nodeCmd represents the node command
@@ -20,16 +20,48 @@ var nodeCmd = &cobra.Command{
 			fmt.Println("Missing docker command")
 			return
 		}
+		fmt.Println(args)
+		// dockerCmd := ""
+		// envVar := false
+		// for _, arg := range args {
+		// 	if envVar {
+		// 		env := strings.Split(arg, "=")
+		// 		if len(env) == 2 {
+		// 			nextArg := strings.Split(env[1], " ")
+		// 			if len(nextArg) >= 2 {
+		// 				newS := strings.Join(env[1:], " ")
+		// 				dockerCmd = dockerCmd + env[0] + "=\"" + newS + "\""
+		// 				envVar = false
+		// 				continue
+		// 			} else {
+		// 				dockerCmd = dockerCmd + " " + env[0] + "=" + env[1]
+		// 				envVar = false
+		// 				continue
+		// 			}
+		// 		} else {
+		// 				newS := strings.Join(env[1:], "=")
+		// 				dockerCmd = dockerCmd + " " + env[0] + "=\"" + newS + "\""
+		// 				envVar = false
+		// 				continue
+		// 		}
+		// 	}
+		// 	if arg == "-e" {
+		// 		envVar = true
+		// 	}
+		// 	dockerCmd = dockerCmd + " " + arg
+		// }
+
 		machine, _ := cmd.Flags().GetString("machine")
-		dockerCmd := strings.Join(args, " ")
-		fmt.Println(dockerCmd)
+		// dockerCmd = strings.Trim(dockerCmd, " ")
+		// fmt.Println(dockerCmd)
 		if args[0] != "docker" {
 			fmt.Println("Only docker is supported")
+			return
 		}
 		client := startClient()
 
 		body, err := json.Marshal(&api.AddNodeRequest{
-			DockerCmd: dockerCmd,
+			DockerCmd: args,
 			MachineId: machine,
 		})
 
